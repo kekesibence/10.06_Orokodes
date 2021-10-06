564b406aa6dc173c2fb4c08cd6fede590c25f6f6
kekesibence <bence.kekesi@gmail.com>
2021-10-06 15:01:08 +0200

    Palack osztály létrehozása

---
+++ src/hu/petrik/Palack.java
@@ -0,0 +1,46 @@
+package hu.petrik;
+
+public class Palack {
+    private String nev;
+    private int maxUrtartalom;
+    private int jelenlegiUrtartalom;
+
+
+    public Palack(String nev, int maxUrtartalom, int jelenlegiUrtartalom) {
+        this.nev = nev;
+        this.maxUrtartalom = maxUrtartalom;
+        this.jelenlegiUrtartalom = jelenlegiUrtartalom;
+    }
+
+    public Palack(String nev, int maxUrtartalom) {
+        this.nev = nev;
+        this.maxUrtartalom = maxUrtartalom;
+        this.jelenlegiUrtartalom = 1;
+    }
+    public int getJelenlegiUrtartalom() {
+        return jelenlegiUrtartalom;
+    }
+
+    public void setJelenlegiUrtartalom(int jelenlegiUrtartalom) {
+        if ( jelenlegiUrtartalom > this.maxUrtartalom) {
+            this.jelenlegiUrtartalom = this.maxUrtartalom;
+        }
+        else {
+            this.jelenlegiUrtartalom = jelenlegiUrtartalom;
+        }
+
+        if (this.jelenlegiUrtartalom == 0) {
+            this.nev=null;
+        }
+    }
+
+    public int suly() {
+        return  this.maxUrtartalom/35 + this.jelenlegiUrtartalom;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Palack, benne levo ital: %s, jelenleg %d ml van benne, maximum \n " +
+        " %s ml fer bele.", this.nev, this.jelenlegiUrtartalom, this.maxUrtartalom);
+    }
+}
